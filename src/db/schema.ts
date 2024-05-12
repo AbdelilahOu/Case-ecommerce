6e3520582303022c9c2f400d768f0014bcfcaeee
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-05-12 09:39:34 +0000

    FEAT: fix drizzle schema and migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,14 +8,15 @@ import {
   real,
   boolean,
 } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
-const orderStatusEnum = pgEnum("order_status_enum", [
+export const orderStatusEnum = pgEnum("status", [
   "fulfilled",
   "shipped",
   "awaiting_shipment",
 ]);
 
-const phoneModelEnum = pgEnum("phone_model_enum", [
+export const phoneModelEnum = pgEnum("model", [
   "iphonex",
   "iphone11",
   "iphone12",
@@ -24,36 +25,36 @@ const phoneModelEnum = pgEnum("phone_model_enum", [
   "iphone15",
 ]);
 
-const caseMaterialEnum = pgEnum("case_material_enum", [
+export const caseMaterialEnum = pgEnum("material", [
   "silicone",
   "polycarbonate",
 ]);
 
-const caseFinishEnum = pgEnum("case_finish_enum", ["smooth", "textured"]);
+export const caseFinishEnum = pgEnum("finish", ["smooth", "textured"]);
 
-const caseColorEnum = pgEnum("case_color_enum", ["black", "blue", "rose"]);
+export const caseColorEnum = pgEnum("color", ["black", "blue", "rose"]);
 
 export const configurations = pgTable("configurations", {
-  id: uuid("id").defaultRandom(),
+  id: uuid("id").defaultRandom().primaryKey(),
   width: integer("width").notNull(),
   height: integer("height").notNull(),
   imageUrl: text("img_url").notNull(),
-  color: caseColorEnum("case_color_enum"),
-  finish: caseFinishEnum("case_finish_enum"),
-  material: caseMaterialEnum("case_material_enum"),
-  model: phoneModelEnum("phone_model_enum"),
+  color: caseColorEnum("color"),
+  finish: caseFinishEnum("finish"),
+  material: caseMaterialEnum("material"),
+  model: phoneModelEnum("model"),
   croppedImageUrl: text("cropped_img_url"),
 });
 
 export const users = pgTable("users", {
-  id: uuid("id").defaultRandom(),
+  id: uuid("id").defaultRandom().primaryKey(),
   email: text("email"),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
 
-export const ShippingAddress = pgTable("shipping_address", {
-  id: uuid("id").defaultRandom(),
+export const shippingAddress = pgTable("shipping_address", {
+  id: uuid("id").defaultRandom().primaryKey(),
   name: text("name"),
   street: text("street"),
   city: text("city"),
@@ -63,8 +64,8 @@ export const ShippingAddress = pgTable("shipping_address", {
   phoneNumber: text("phone_number"),
 });
 
-export const BillingAddress = pgTable("billing_address", {
-  id: uuid("id").defaultRandom(),
+export const billingAddress = pgTable("billing_address", {
+  id: uuid("id").defaultRandom().primaryKey(),
   name: text("name"),
   street: text("street"),
   city: text("city"),
@@ -73,20 +74,57 @@ export const BillingAddress = pgTable("billing_address", {
   state: text("state"),
   phoneNumber: text("phone_number"),
 });
+
+export const configurationsRelations = relations(configurations, ({ one }) => ({
+  orders: one(orders),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  orders: many(orders),
+}));
+
+export const shippingAddressRelations = relations(
+  shippingAddress,
+  ({ many }) => ({
+    orders: many(orders),
+  })
+);
+
+export const BillingAddressRelations = relations(
+  billingAddress,
+  ({ many }) => ({
+    orders: many(orders),
+  })
+);
 
 export const orders = pgTable("orders", {
-  id: uuid("id").defaultRandom(),
+  id: uuid("id").defaultRandom().primaryKey(),
   configurationId: uuid("configuration_id").references(() => configurations.id),
-  userId: uuid("uder_id").references(() => users.id),
+  userId: uuid("user_id").references(() => users.id),
   amount: real("amount"),
   isPaid: boolean("is_paid").default(false),
-  status: orderStatusEnum("order_status_enum"),
+  status: orderStatusEnum("status"),
   shippingAddressId: uuid("shipping_address_id").references(
-    () => ShippingAddress.id
+    () => shippingAddress.id
   ),
-  billingAddress: uuid("billing_address_id").references(
-    () => BillingAddress.id
+  billingAddressId: uuid("billing_address_id").references(
+    () => billingAddress.id
   ),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export const ordersRelations = relations(orders, ({ one }) => ({
+  user: one(users, {
+    fields: [orders.userId],
+    references: [users.id],
+  }),
+  billingAddress: one(billingAddress, {
+    fields: [orders.billingAddressId],
+    references: [billingAddress.id],
+  }),
+  shippingAddress: one(shippingAddress, {
+    fields: [orders.shippingAddressId],
+    references: [shippingAddress.id],
+  }),
+}));
---
+++ src/drizzle/0000_woozy_shiver_man.sql
@@ -0,0 +1,107 @@
+DO $$ BEGIN
+ CREATE TYPE "color" AS ENUM('black', 'blue', 'rose');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "finish" AS ENUM('smooth', 'textured');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "material" AS ENUM('silicone', 'polycarbonate');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "status" AS ENUM('fulfilled', 'shipped', 'awaiting_shipment');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "model" AS ENUM('iphonex', 'iphone11', 'iphone12', 'iphone13', 'iphone14', 'iphone15');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "billing_address" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"street" text,
+	"city" text,
+	"postal_code" text,
+	"country" text,
+	"state" text,
+	"phone_number" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "configurations" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"width" integer NOT NULL,
+	"height" integer NOT NULL,
+	"img_url" text NOT NULL,
+	"color" "color",
+	"finish" "finish",
+	"material" "material",
+	"model" "model",
+	"cropped_img_url" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "orders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"configuration_id" uuid,
+	"user_id" uuid,
+	"amount" real,
+	"is_paid" boolean DEFAULT false,
+	"status" "status",
+	"shipping_address_id" uuid,
+	"billing_address_id" uuid,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "shipping_address" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"street" text,
+	"city" text,
+	"postal_code" text,
+	"country" text,
+	"state" text,
+	"phone_number" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" text,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "orders" ADD CONSTRAINT "orders_configuration_id_configurations_id_fk" FOREIGN KEY ("configuration_id") REFERENCES "configurations"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "orders" ADD CONSTRAINT "orders_shipping_address_id_shipping_address_id_fk" FOREIGN KEY ("shipping_address_id") REFERENCES "shipping_address"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "orders" ADD CONSTRAINT "orders_billing_address_id_billing_address_id_fk" FOREIGN KEY ("billing_address_id") REFERENCES "billing_address"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
